8b0fde897acbb650129f65706e66e45327c0a170
janlou2 <atefehazimi94@gmail.com>
2016-02-26 15:01:27 +0330

    Update addadmin.lua

--- plugins/addadmin.lua
+++ plugins/addadmin.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches)
-local sudo = 111984481,103373495,67647823
+local sudo = 111984481
     if matches[1]:lower() == "مدیرو بیار" then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
     end
