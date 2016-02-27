5f7528d7c5af90d877f21be51efac4206a8b6481
janlou2 <atefehazimi94@gmail.com>
2016-02-27 16:08:19 +0330

    Update welcome.lua

--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -58,7 +58,7 @@ local function description_rules(msg, nama)
          rules = data[tostring(msg.to.id)]["rules"]
          rules = "\nقوانین:\n"..rules.."\n"
       end
-      local sambutan = "سلام .  "..nama.."\nخوشومدی به  '"..string.gsub(msg.to.print_name, "_", " ").."'\nاز قوانین و حرف مدیرا یادت نره پیروی کنی . برای دیدن دستوراتم بنویس راهنما\n"
+      local sambutan = "سلام   "..nama.."\nخوشومدی به  '"..string.gsub(msg.to.print_name, "_", " ").."'"
       local text = sambutan..about..rules.."\n"
       local receiver = get_receiver(msg)
       send_large_msg(receiver, text, ok_cb, false)
