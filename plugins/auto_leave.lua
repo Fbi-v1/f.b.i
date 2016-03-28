2debbc3c157fbc455b8ffa37db94685c621466e5
janlou2 <atefehazimi94@gmail.com>
2016-03-28 10:58:04 +0430

    Update auto_leave.lua

--- plugins/auto_leave.lua
+++ plugins/auto_leave.lua
@@ -8,7 +8,7 @@ local function run(msg)
   if msg.service and msg.action.type == 'chat_add_user' then
     local data = load_data(_config.moderation.data)
     if not data[tostring(msg.to.id)] then
-      print "ین گروه متعلق به گروه های بنده نمیباشد . بای ..."
+      print "این گروه متعلق به گروه های بنده نمیباشد . بای ..."
       chat_del_user('chat#id'..msg.to.id, 'user#id'..our_id, callback, false)
     end
   end
