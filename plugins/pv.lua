d28b2a07a1be7e51828fabd9b84baf45467fc577
Dr.Cyber <amirhosseinaghajani@hotmail.com>
2016-03-31 12:57:37 +0430

    Update pv.lua

--- plugins/pv.lua
+++ plugins/pv.lua
@@ -1,6 +1,6 @@
 function run(msg, matches)
 if msg.to.type == 'user' and not is_admin(msg) then
-return "سلام اینجا پیوی رباته اگه کاری دارید:\n@janlou\n@bestroid_admin\n@amiraliiiiiiiii81"
+return "سلام اینجا پیوی رباته اگه کاری دارید:\n@DrCyber1736  @redteam_01_25"
 end
 end
 return {
