fab901bef9d7e173e93be881ccb8666156164750
Dr.Cyber <amirhosseinaghajani@hotmail.com>
2016-03-31 11:14:46 +0430

    Update teleadvan.lua

--- bot/teleadvan.lua
+++ bot/teleadvan.lua
@@ -255,12 +255,11 @@ function create_config( )
     
      Hello my Good friends 
      
-    ‼️ this bot is made by : @creed_is_dead
+    ‼️ this bot is made by :Dr.Cyber
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+   ߔࠀ   @DrCyber1736
+   ߔࠀ   @redteam_01_25
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
@@ -411,9 +410,8 @@ function create_config( )
 ❗️Note that: از ! و / استفاده نکنید
 ●•••••••••••••••••••••••••●
 Developers: 
-@janlou
-@amiraliiiiiiiii81
-@bestroid_admin
+@DrCyber1736
+@redteam_01_25
 ]],
   }
   serialize_to_file(config, './data/config.lua')
